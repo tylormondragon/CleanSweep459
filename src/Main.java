7caacbcf5b76b84459aeec5f9bd09d9ad5c3c626
asmaasfak <51274482+asmaasfak@users.noreply.github.com>
2019-10-17 19:10:26 -0500

    asma src folder

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+public static void main(String[] args) {
+	int[][] room = new int[3][3];
+		System.out.println("SE 459");
+		System.out.println("Group 2");
+
+		System.out.print("This is our Clean Sweep Vacuum ");
+		System.out.println("\n");	
+		Vacuum vacuum = new Vacuum(room);
+	}
+}
---
+++ src/Vacuum.java
@@ -0,0 +1,78 @@
+import java.lang.reflect.Array;
+
+public class Vacuum {
+	int[][] room;
+	
+	int[] position = new int [2];
+	
+	int  i = (int) Array.get(position, 0);
+	int  j = (int) Array.get(position, 1);
+
+
+
+	public Vacuum(int[][] room2) {
+		// TODO Auto-generated constructor stub
+		room = room2;
+		LookUpLocation lookup = new LookUpLocation(this, position, i, j);
+
+	}
+	public void initVacuum(){
+		this.setColumn(0);
+		this.setRow(0);
+		
+		System.out.println("Clean Sweep on");
+	}
+	public void setRow(int y) {
+		// TODO Auto-generated method stub
+		if (y >= 0 && y <= room.length) {
+			i = y;
+		position [1] = i;
+		}
+		else {
+			if (y < 0) {
+			i = y + 1;
+			position [1] = i;
+			}
+			if (y > room.length) {
+				i = y - 1;
+				position [1] = i;
+			}
+		}
+	}
+	
+	public int getRow() {
+		return i;
+	}
+		
+	public void setColumn(int x) {
+		// TODO Auto-generated method stub
+		if (x >= 0 && x <= room.length) {
+			j = x;
+			position [0] = j;
+		} 
+		else {
+			if (x < 0) {
+			j = x + 1;
+			position [0] = j;
+			}
+			if (x > room.length) {
+				i = x - 1;
+				position [0] = j;
+			}
+		}
+	}
+	
+	public int getColumn() {
+		return j;
+	}
+	
+	public void currentLocation (int r, int c) {
+		r = getRow();
+		c = getColumn();
+		position = new int [] {r, c};
+	}
+	public int[] getCurrentLocation () {
+		return position;
+	}
+
+}
