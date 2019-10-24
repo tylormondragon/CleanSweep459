37a6974f7c1205ca3796d7e7f11d8977b96f2f72
rtalukder <rtalukder@forsythe.com>
2019-10-24 18:25:43 -0500

    refactoring

--- src/main/java/SensorSimulator/SensorSimulator.java
+++ src/main/java/SensorSimulator/SensorSimulator.java
@@ -1,13 +1,14 @@
 package main.java.SensorSimulator;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class SensorSimulator {
     public ArrayList<SensorObject> sensorObjectArrayList = new ArrayList<>();
