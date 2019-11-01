f015d6f7ff11435a08d1b8f1e7f73468c13d8918
Tylor Mondragon <twaitsmo@alumni.nd.edu>
2019-11-01 14:46:24 -0500

    starting my updates to monitor power control

--- src/main/java/ControlSystem/Power.java
+++ src/main/java/ControlSystem/Power.java
@@ -1,8 +1,32 @@
 package main.java.ControlSystem;
 
+import main.java.SensorSimulator.SensorObject;
+
 public class Power {
+
+	private SensorObject floorTile;
 	
 	float power;
+
+	public Double calculateMovementPower(String floorType1, String floorType2) {
+		Double num = 8.0;
+		floorType1 = floorTile.getFloorType();
+		floorType2 = floorTile.getFloorType();
+
+		return num;
+
+	}
+
+	public Double calculateCleaningPower(String floorType) {
+		Double num = 1.0;
+
+		return num;
+	}
+
+	private int getPowerValue() {
+		int num = 8;
+		return num;
+	}
 
 	public Power(float power) {
 		this.power = power;
