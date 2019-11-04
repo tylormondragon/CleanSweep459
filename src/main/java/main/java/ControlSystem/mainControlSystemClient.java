77bbdaa96f78b17a524268416b287c539ba87bd9
asmaasfak <51274482+asmaasfak@users.noreply.github.com>
2019-11-03 18:43:07 -0600

    Changing room size

--- src/main/java/main/java/ControlSystem/mainControlSystemClient.java
+++ src/main/java/main/java/ControlSystem/mainControlSystemClient.java
@@ -14,7 +14,7 @@ public class mainControlSystemClient {
     public static void main(String[] args) {
         ControlSystemClient client = new ControlSystemClient();
 
-        int[][] room = new int[3][3];
+        int[][] room = new int[10][10];
         power = new Power(250.0);
         Logger logger = Logger.getInstance();
         Logger.logInfo("SE 459");
