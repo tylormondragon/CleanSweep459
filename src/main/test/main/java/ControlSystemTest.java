db194fb3133e43a93e947fce12d3b8b05af44da5
rtalukder@forsythe.com <rtalukder@forsythe.com>
2019-11-03 19:36:52 -0600

    added maven framework and added pom file

---
+++ src/main/test/main/java/ControlSystemTest.java
@@ -0,0 +1,15 @@
+package main.java;
+
+/**
+ * Class: SE<xxx> - <descrption>
+ * Author: Raquib Talukder
+ **/
+
+import org.junit.jupiter.api.*;
+
+public class ControlSystemTest {
+    @Test
+    public void Test(){
+        System.out.println("test");
+    }
+}
